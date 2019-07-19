7125dc506153c2e0b7ad3673799a13b7d28550a0
dothiphu <dophu3196@gmail.com>
2019-07-19 08:38:03 +0700

    Giao diện 2

---
+++ Template/Template/Controllers/KhachHangController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Template.Models;
+
+
+namespace Template.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        
+        //
+        // GET: /KhachHang/
+        public ActionResult KhachHang()
+        {
+            return View();
+        }
+       
+	}
+}
---
+++ Template/Template/Controllers/LayoutNguoiDungController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template.Controllers
+{
+    public class LayoutNguoiDungController : Controller
+    {
+        //
+        // GET: /LayoutNguoiDung/
+        public ActionResult LayoutNguoiDung()
+        {
+            return View();
+        }
+	}
+}
