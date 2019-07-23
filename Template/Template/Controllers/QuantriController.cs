6eaf8fc94e4043abbd1f7cdb13c0b20ad66ee2be
Cao Ngoc The <caongocthe19@gmail.com>
2019-07-23 09:15:17 +0700

    them phan login

---
+++ Template/Template/Controllers/QuantriController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Template.Models;
+
+
+namespace Template.Controllers
+{
+    public class QuantriController : Controller
+    {
+        // GET: Quantri
+        QuanLiThuePhongEntities db = new QuanLiThuePhongEntities();
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                //var kiemtradangky = new KiemtradangkyController();
+                //if (kiemtradangky.CheckUserName(user.Username))
+                //{
+                //    ModelState.AddModelError("", "Tên đăng nhập đã tồn tại");
+                //}
+                //chèn dữ liệu
+                db.User.Add(user);
+                //Lưu vào cơ sở dữ liệu
+                db.SaveChanges();
+            }
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection f)
+        {
+            string sTaikhoan = f["txtTaikhoan"].ToString();
+            string sMatkhau = f.Get("txtMatkhau").ToString();
+            User user = db.Users.SingleOrDefault(n => n.Username == sTaikhoan && n.Password == sMatkhau);
+            if (user != null)
+            {
+                
+                
+                Session["Taikhoan"] = user;
+                return RedirectToAction("Admin", "LayoutAdmin");              
+            }
+            ViewBag.ThongBao = "Thất bại";
+            return View();
+        }
+    }
+}
---
+++ Template/Template/kiemtradangky/Kiemtradangky.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Template.Models;
+
+namespace Template.Kiemtradangky
+{
+    public class KiemtradangkyController : Controller
+    {
+        // GET: Kiemtradangky
+        QuanLiThuePhongEntities db = new QuanLiThuePhongEntities();
+        public bool CheckUserName(string Username)
+        {
+            return db.User.Count(x => x.Username == Username) > 0;
+        }
+    }
+}
